9e83b21783f5cd5bfce3b2686c9d4c7c5a7b574d
LinusLundberg123 <linus.lundberg0108@gmail.com>
2025-02-11 16:13:00 +0100

    Added gitignore

--- Test/Lab2Test.java
+++ Test/Lab2Test.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.*;
 
 public class TruckTest {
@@ -8,6 +10,8 @@ public class TruckTest {
     private CarTransport transport;
     private Saab95 car1;
     private Volvo240 car2;
+    private AutoRepairShop<Car> auto1;
+    private AutoRepairShop<Saab95> auto2;
 
     @Before
     public void setUp() {
@@ -15,6 +19,8 @@ public class TruckTest {
         transport = new CarTransport(2, "TestTransport");
         car1 = new Saab95();
         car2 = new Volvo240();
+        auto1 = new AutoRepairShop<Car>(1,"numb1");
+        auto2 = new AutoRepairShop<Saab95>(2, "numb2");
         car1.x = 1;
         car1.y = 1;
     }
@@ -65,5 +71,19 @@ public class TruckTest {
         // Check if car position updates with transport position
         assertEquals(transport.x, car1.x, 0.0);
         assertEquals(transport.y, car1.y, 0.0);
+    }
+
+    @Test
+    public void testAutoRepair() {
+        auto1.load(car1);
+        assertThrows(IllegalArgumentException.class, () -> auto1.load(car2));
+        assertEquals(1, auto1.getMaxCars());
+        assertEquals("numb1", auto1.getShopName());
+        assertEquals(car1, auto1.offLoad());
+        assertFalse(auto1.getLoadedCars().contains(car1));
+        auto2.load(car1);
+        assertTrue(auto2.getLoadedCars().contains(car1));
+        auto2.removeSpecific(car1);
+        assertFalse(auto2.getLoadedCars().contains(car1));
     }
 }
